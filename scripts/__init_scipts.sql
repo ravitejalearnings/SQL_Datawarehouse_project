811c277b0fd51c1a03af933d9556f7f4950a8928
Ravi Teja Anumandla <ravianumandla69@gmail.com>
2025-06-24 11:56:11 +0530

    Create __init_scipts.sql

---
+++ scripts/__init_scipts.sql
@@ -0,0 +1,12 @@
+drop database if exists datawarehouse;
+create database datawarehouse;
+use datawarehouse
+
+drop schema if exists bronze;
+create schema bronze;
+
+drop schema if exists silver;
+create schema silver;
+
+drop schema if exists gold;
+create schema gold;
